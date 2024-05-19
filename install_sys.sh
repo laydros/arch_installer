6c773b064a28b1cd40dda96c8f7d3de1f7d4b0f8
Jason Hamilton <git@laydros.net>
2024-05-19 15:05:46 -0400

    added initial gitignore, more in install_sys.sh

--- install_sys.sh
+++ install_sys.sh
@@ -30,3 +30,46 @@ dialog --title "Choose your hard drive" --no-cancel --radiolist \
     15 60 4 "${devices_list[@]}" 2> hd
 
 hd=$(cat hd) && rm hd
+
+default_size="8"
+dialog --no-cancel --inputbox \
+    "You need three partitions: Boot, Root, and Swap \n\
+    The boot partition will be 1024M \n\
+    The root partition will be the remaining of the hard disk \n\n\
+    Enter below the desired partition size (in Gb) for the Swap. \n\n\
+    If you don't enter anything, it will default to ${default_size}G. \n" \
+    20 60 2> swap_size
+    size=$(cat swap_size) && rm swap_size
+
+    [[ $size =~ ^[0-9]+$ ]] || size=$default_size
+
+dialog --no-cancel \
+    -- title "!!! DELETE EVERYTHING !!!" \
+    --menu "Choose the way you'll wipe the hard disk ($hd)" \
+    15 60 4 \
+    1 "Use dd (wipe all disk)" \
+    2 "Use shred (slow & secure)" \
+    3 "No need - my disk is empty" 2> eraser
+
+hderaser=$(cat eraser); rm eraser
+
+function eraseDisk() {
+    case $1 in
+        1) dd if=/dev/zero of="$hd" status=progress 2>&1 \
+            | dialog \
+            --title "formatting $hd..." \
+            --progressbox --stdout 20 60 ;;
+        2) shred -v "$hd" \
+            | dialog \
+            --title "Formatting $hd..." \
+            --progressbox --stdout 20 60;;
+        3) ;;
+    esac
+}
+
+# eraseDisk "$hderaser"
+
+boot_partition_type=1
+[[ "$uefi" == 0 ]] && boot_partition_type=4
+
+
