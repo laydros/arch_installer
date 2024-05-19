acd5f6087c4021b0bdee8051480608aba255b059
Jason Hamilton <git@laydros.net>
2024-05-19 17:46:29 -0400

    adding more of the install_apps.sh script

--- install_apps.sh
+++ install_apps.sh
@@ -7,9 +7,10 @@ curl https://raw.githubusercontent.com/laydros\
 /arch_installer/main/apps.csv > $apps_path
 
 dialog --title "Welcome!" \
-    --msgbox "Welcome to the installation script for your apps and dotfiles!" \
+--msgbox "Welcome to the install script for your apps and dotfiles!" \
     10 60
 
+# Allow the user to select the group of packages he (or she) wants to install.
 apps=("essential" "Essentials" on
       "network" "Network" on
       "tools" "Nice tools to have (highly recommended)" on
@@ -32,4 +33,39 @@ You can select an option with SPACE and valid your choices with ENTER." \
 "${apps[@]}" 2> app_choices
 
 choices=$(cat app_choices) && rm app_choices
+
+# Create a regex to only select the packages we want
+selection="^$(echo $choices | sed -e 's/ /,|^/g'),"
+lines=$(grep -E "$selection" "$apps_path")
+count=$(echo "$lines" | wc -l)
+packages=$(echo "$lines" | awk -F, {'print $2'})
+echo "$selection" "$lines" "$count" >> "/tmp/packages"
+
+pacman -Syu --noconfirm
+
+rm -f /tmp/aur_queue
+
+dialog --title "Let's go!" --msgbox \
+"The system will not install everything your need.\n\n\
+It will take some time.\n\n" \
+13 60
+
+c=0
+echo "$packages" | while read -r line; do
+    c=$(( "$c" + 1 ))
+
+    dialog --title "Arch Linux Installation" --infobox \
+    "Downloading and installing program $c out of $count: $line..." \
+    8 70
+    ((pacman --noconfirm --needed -S "$line" > /tmp/arch_install 2>&1) \
+    || echo "$line" >> /tmp/aur_queue) \
+    || echo "$line" >> /tmp/arch_install_failed
+    if [ "$line" = "zsh" ]; then
+        # Set Zsh as default terminal for our user
+        chsh -s "$(which zsh)" "$name"
+    fi
+    if [ "$line" = "networkmanager" ]; then
+        systemctl enable NetworkManager.service
+    fi
+done
 
